681b5642059c48b4e4c9b26c6867e5bbe9849e48
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-02-01 14:11:35 -0800

    writer tests

---
+++ tests/Assetic/Test/AssetWriterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Assetic package.
+ *
+ * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Test;
+
+use Assetic\AssetWriter;
+
+class AssetWriterTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->dir = sys_get_temp_dir().'/assetic_tests_'.rand(11111, 99999);
+        mkdir($this->dir);
+        $this->writer = new AssetWriter($this->dir);
+    }
+
+    protected function tearDown()
+    {
+        array_map('unlink', glob($this->dir.'/*'));
+        rmdir($this->dir);
+    }
+
+    public function testWriteManagerAssets()
+    {
+        $asset = $this->getMock('Assetic\\Asset\\AssetInterface');
+        $am = $this->getMock('Assetic\\AssetManager');
+
+        $am->expects($this->once())
+            ->method('all')
+            ->will($this->returnValue(array($asset)));
+        $asset->expects($this->once())
+            ->method('getTargetUrl')
+            ->will($this->returnValue('target_url'));
+        $asset->expects($this->once())
+            ->method('dump')
+            ->will($this->returnValue('content'));
+
+        $this->writer->writeManagerAssets($am);
+
+        $this->assertFileExists($this->dir.'/target_url');
+        $this->assertEquals('content', file_get_contents($this->dir.'/target_url'));
+    }
+}
