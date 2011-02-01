47cc46d68e4b55547cdbb43b537ac09295902a89
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-02-01 08:27:30 -0800

    added a class for writing assets to the filesystem

---
+++ src/Assetic/AssetWriter.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Assetic package.
+ *
+ * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic;
+
+use Assetic\Asset\AssetInterface;
+
+/**
+ * Writes assets to the filesystem.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
+class AssetWriter
+{
+    private $dir;
+
+    /**
+     * Constructor.
+     *
+     * @param string $dir The base web directory
+     */
+    public function __construct($dir)
+    {
+        $this->dir = $dir;
+    }
+
+    public function writeManager(AssetManager $am)
+    {
+        foreach ($am->all() as $asset) {
+            $this->writeAsset($asset);
+        }
+    }
+
+    public function writeAsset(AssetInterface $asset)
+    {
+        static::write($this->dir . '/' . $asset->getTargetUrl(), $asset->dump());
+    }
+
+    static protected function write($path, $contents)
+    {
+        file_put_contents($path, $contents);
+    }
+}
