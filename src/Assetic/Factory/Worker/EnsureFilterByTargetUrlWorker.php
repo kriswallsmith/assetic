5c3d98324ab2ac31ec53af87fd0ef3a78d6c3e94
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-02-07 08:10:21 -0800

    added a factory worker that ensures filters based on an asset's target url

---
+++ src/Assetic/Factory/Worker/EnsureFilterByTargetUrlWorker.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Assetic package.
+ *
+ * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Factory\Worker;
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Filter\FilterInterface;
+
+/**
+ * Applies a filter to an asset based on a target URL match.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
+class EnsureFilterByTargetUrlWorker implements WorkerInterface
+{
+    private $pattern;
+    private $filter;
+
+    /**
+     * Constructor.
+     *
+     * @param string          $pattern A regex for checking the asset's target URL
+     * @param FilterInterface $filter  A filter to apply if the regex matches
+     */
+    public function __construct($pattern, FilterInterface $filter)
+    {
+        $this->pattern = $pattern;
+        $this->filter = $filter;
+    }
+
+    /**
+     * Processes an asset.
+     *
+     * @param AssetInterface $asset An asset
+     */
+    public function process(AssetInterface $asset)
+    {
+        if (preg_match($this->pattern, $asset->getTargetUrl())) {
+            $asset->ensureFilter($this->filter);
+        }
+    }
+}
---
+++ tests/Assetic/Test/Factory/Worker/EnsureFilterByTargetUrlWorkerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Assetic package.
+ *
+ * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Test\Factory\Worker;
+
+use Assetic\Factory\Worker\EnsureFilterByTargetUrlWorker;
+
+class EnsureFilterByTargetUrlWorkerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMatch()
+    {
+        $filter = $this->getMock('Assetic\\Filter\\FilterInterface');
+        $asset = $this->getMock('Assetic\\Asset\\AssetInterface');
+
+        $asset->expects($this->once())
+            ->method('getTargetUrl')
+            ->will($this->returnValue('css/main.css'));
+        $asset->expects($this->once())
+            ->method('ensureFilter')
+            ->with($filter);
+
+        $worker = new EnsureFilterByTargetUrlWorker('/\.css$/', $filter);
+        $worker->process($asset);
+    }
+
+    public function testNonMatch()
+    {
+        $filter = $this->getMock('Assetic\\Filter\\FilterInterface');
+        $asset = $this->getMock('Assetic\\Asset\\AssetInterface');
+
+        $asset->expects($this->once())
+            ->method('getTargetUrl')
+            ->will($this->returnValue('js/all.js'));
+        $asset->expects($this->never())->method('ensureFilter');
+
+        $worker = new EnsureFilterByTargetUrlWorker('/\.css$/', $filter);
+        $worker->process($asset);
+    }
+}
