49b231ab87a64fe4abf24ef9e749664518b3c333
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-02-19 07:26:53 -0800

    added some coverage for the new lazy am

--- tests/Assetic/Test/Factory/LazyAssetManagerTest.php
+++ tests/Assetic/Test/Factory/LazyAssetManagerTest.php
@@ -11,9 +11,44 @@
 
 namespace Assetic\Test\Factory;
 
+use Assetic\Factory\LazyAssetManager;
+
 class LazyAssetManagerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testNothing()
+    protected function setUp()
     {
+        $this->factory = $this->getMockBuilder('Assetic\\Factory\\AssetFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->loader = $this->getMock('Assetic\\Factory\\Loader\\FormulaLoaderInterface');
+
+        $this->am = new LazyAssetManager($this->factory, $this->loader);
+    }
+
+    public function testGetFromLoader()
+    {
+        $resource = $this->getMock('Assetic\\Factory\\Resource\\ResourceInterface');
+        $asset = $this->getMock('Assetic\\Asset\\AssetInterface');
+
+        $formula = array(
+            array('js/core.js', 'js/more.js'),
+            array('?yui_js'),
+            array('output' => 'js/all.js')
+        );
+
+        $this->loader->expects($this->once())
+            ->method('load')
+            ->with($resource)
+            ->will($this->returnValue(array('foo' => $formula)));
+        $this->factory->expects($this->once())
+            ->method('createAsset')
+            ->with($formula[0], $formula[1], $formula[2] + array('name' => 'foo'))
+            ->will($this->returnValue($asset));
+
+        $this->am->addResource($resource);
+        $this->assertSame($asset, $this->am->get('foo'), '->get() returns an asset from the loader');
+
+        // test the "once" expectations
+        $this->am->get('foo');
     }
 }
