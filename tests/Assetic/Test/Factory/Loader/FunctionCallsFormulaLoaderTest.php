76c037f066bb10837dcf29edb52a929321d9cc8f
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-04 16:23:26 +0100

    added a test for the php formula loader

---
+++ tests/Assetic/Test/Factory/Loader/FunctionCallsFormulaLoaderTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Assetic package.
+ *
+ * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Test\Factory\Loader;
+
+use Assetic\Factory\Loader\FunctionCallsFormulaLoader;
+
+class FunctionCallsFormulaLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getInputs
+     */
+    public function testInput($inputs, $name, $expected)
+    {
+        $resource = $this->getMock('Assetic\\Factory\\Resource\\ResourceInterface');
+        $factory = $this->getMockBuilder('Assetic\\Factory\\AssetFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $resource->expects($this->once())
+            ->method('getContent')
+            ->will($this->returnValue('<?php assetic_assets('.$inputs.') ?>'));
+        $factory->expects($this->once())
+            ->method('generateAssetName')
+            ->will($this->returnValue($name));
+
+        $loader = new FunctionCallsFormulaLoader($factory);
+        $formulae = $loader->load($resource);
+
+        $this->assertEquals($expected, $formulae);
+    }
+
+    public function getInputs()
+    {
+        return array(
+            array('"js/core.js"',        'asdf', array('asdf' => array(array('js/core.js'), array(), array()))),
+            array("'js/core.js'",        'asdf', array('asdf' => array(array('js/core.js'), array(), array()))),
+            array("array('js/core.js')", 'asdf', array('asdf' => array(array('js/core.js'), array(), array()))),
+            array('array("js/core.js")', 'asdf', array('asdf' => array(array('js/core.js'), array(), array()))),
+        );
+    }
+}
