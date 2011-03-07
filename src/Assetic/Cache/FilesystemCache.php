136d3866163c7a2d430c24c79bc6fa51a43dbb12
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-07 10:47:18 -0800

    updated copyright information

--- src/Assetic/Cache/FilesystemCache.php
+++ src/Assetic/Cache/FilesystemCache.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Assetic package.
+ * This file is part of the Assetic package, an OpenSky project.
  *
- * (c) Kris Wallsmith <kris.wallsmith@gmail.com>
+ * (c) 2010-2011 OpenSky Project Inc
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
