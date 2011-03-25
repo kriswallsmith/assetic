aebc7fb91ba4bb66bea2ccdb067e5e86fb58593d
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-25 07:05:03 -0700

    fixed autoload of base test classes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,7 +11,13 @@
 
 spl_autoload_register(function($class)
 {
-    if (0 === strpos($class, 'Assetic\\')) {
+    if (0 === strpos($class, 'Assetic\\Test\\')) {
+        $file = __DIR__ . '/../test/' . str_replace('\\', '/', $class) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return true;
+        }
+    } elseif (0 === strpos($class, 'Assetic\\')) {
         $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
         if (file_exists($file)) {
             require_once $file;
