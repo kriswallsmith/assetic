7945b1b375b6ce9d4065e4e06c3eb3c72ae98a7e
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-05-15 10:01:26 -0700

    updated EnsureFilterWorker to check both source and target paths, with a way to specify one or the other

--- src/Assetic/Factory/Worker/EnsureFilterWorker.php
+++ src/Assetic/Factory/Worker/EnsureFilterWorker.php
@@ -15,31 +15,45 @@ use Assetic\Asset\AssetInterface;
 use Assetic\Filter\FilterInterface;
 
 /**
- * Applies a filter to an asset based on a target URL match.
+ * Applies a filter to an asset based on a source and/or target path match.
  *
  * @author Kris Wallsmith <kris.wallsmith@gmail.com>
  * @todo A better asset-matcher mechanism
  */
 class EnsureFilterWorker implements WorkerInterface
 {
+    const CHECK_SOURCE = 1;
+    const CHECK_TARGET = 2;
+
     private $pattern;
     private $filter;
+    private $flags;
 
     /**
      * Constructor.
      *
      * @param string          $pattern A regex for checking the asset's target URL
      * @param FilterInterface $filter  A filter to apply if the regex matches
+     * @param integer         $flags   Flags for what to check
      */
-    public function __construct($pattern, FilterInterface $filter)
+    public function __construct($pattern, FilterInterface $filter, $flags = null)
     {
+        if (null === $flags) {
+            $flags = self::CHECK_SOURCE | self::CHECK_TARGET;
+        }
+
         $this->pattern = $pattern;
         $this->filter = $filter;
+        $this->flags = $flags;
     }
 
     public function process(AssetInterface $asset)
     {
-        if (0 < preg_match($this->pattern, $asset->getTargetPath())) {
+        if (
+            (self::CHECK_SOURCE === (self::CHECK_SOURCE & $this->flags) && preg_match($this->pattern, $asset->getSourcePath()))
+            ||
+            (self::CHECK_TARGET === (self::CHECK_TARGET & $this->flags) && preg_match($this->pattern, $asset->getTargetPath()))
+        ) {
             $asset->ensureFilter($this->filter);
         }
     }
