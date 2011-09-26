0b73288a8f88bcadf48e2e6dc24a3bedab9616e6
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-09-26 15:53:27 -0700

    fixed constructor signature

--- src/Assetic/Exception/FilterException.php
+++ src/Assetic/Exception/FilterException.php
@@ -40,7 +40,7 @@ class FilterException extends \RuntimeException implements Exception
         return new self($message);
     }
 
-    public function __construct($message, $code, \Exception $previous = null)
+    public function __construct($message, $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
 
