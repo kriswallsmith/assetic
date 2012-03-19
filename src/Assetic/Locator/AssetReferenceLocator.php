f20326a4f29548a85f25d79447933a89d32fd612
everzet <ever.zet@gmail.com>
2012-03-19 14:29:48 +0200

    implemented asset reference locator

---
+++ src/Assetic/Locator/AssetReferenceLocator.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2011 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Locator;
+
+use Assetic\AssetManager;
+use Assetic\Asset\AssetReference;
+
+/**
+ * Transforms asset manager reference string into asset.
+ *
+ * @author Konstantin Kudryashov <ever.zet@gmail.com>
+ */
+class AssetReferenceLocator implements AssetLocatorInterface
+{
+    private $am;
+
+    /**
+     * Constructor.
+     *
+     * @param AssetManager $am The asset manager
+     */
+    public function __construct(AssetManager $am)
+    {
+        $this->am = $am;
+    }
+
+    /**
+     * Parses an input string into an asset.
+     *
+     * @param string $input   An input string
+     * @param array  $options An array of options
+     *
+     * @return AssetInterface An asset
+     */
+    public function locate($input, array $options = array())
+    {
+        if ('@' == $input[0]) {
+            return $this->createAssetReference(substr($input, 1));
+        }
+    }
+
+    protected function createAssetReference($name)
+    {
+        if (!$this->am) {
+            throw new \LogicException('There is no asset manager.');
+        }
+
+        return new AssetReference($this->am, $name);
+    }
+}
