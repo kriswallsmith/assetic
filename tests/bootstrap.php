2b0bb42c48174bfebdb4c14387cf6d684a414bd1
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-05-09 16:47:48 -0700

    initial setup

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2012 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
+    echo <<<EOM
+You must set up the project dependencies by running the following commands:
+
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+
+EOM;
+
+    exit(1);
+}
+
+$loader->add('Assetic\Test', __DIR__);
