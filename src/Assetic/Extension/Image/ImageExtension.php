f2cf454d4aeb5da09d59c47f03bd439308a5a941
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-05-19 07:08:37 -0700

    added initial image extension

---
+++ src/Assetic/Extension/Image/ImageExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2012 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Extension\Image;
+
+use Assetic\AbstractExtension;
+use Assetic\EnvironmentInterface;
+use Assetic\Extension\Image\ImageProcessor;
+
+class ImageExtension extends AbstractExtension
+{
+    private $processors;
+
+    public function __construct()
+    {
+        $this->processors = array();
+    }
+
+    public function initialize(EnvironmentInterface $env)
+    {
+        $env->getExtension('core')
+            ->registerMimeType('gif', 'image/gif')
+            ->registerMimeType('jpeg', 'image/jpeg')
+            ->registerMimeType('jpg', 'image/jpeg')
+            ->registerMimeType('png', 'image/png')
+            ->registerPostProcessor(new ImageProcessor($this), 'image/*')
+        ;
+    }
+
+    public function registerImageOptimizer($mimeType, ProcessorInterface $processor)
+    {
+        $this->processors[$mimeType] = $processor;
+    }
+
+    public function getImageOptimizer($mimeType)
+    {
+        if (isset($this->processors[$mimeType])) {
+            return $this->processors[$mimeType];
+        }
+    }
+
+    public function getName()
+    {
+        return 'image';
+    }
+}
---
+++ src/Assetic/Extension/Image/Processor/ImageProcessor.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2012 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Extension\Image\Processor;
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Extension\Core\Processor\ProcessorInterface;
+use Assetic\Extension\Image\ImageExtension;
+
+class ImageProcessor implements ProcessorInterface
+{
+    private $extension;
+
+    public function __construct(ImageExtension $extension)
+    {
+        $this->extension = $extension;
+    }
+
+    public function process(AssetInterface $asset)
+    {
+        $mimeType = $asset->getAttribute('mime_type');
+
+        if ($mimeType && $processor = $this->extension->getImageOptimizer($mimeType)) {
+            $processor->process($asset);
+        }
+    }
+}
