ababbab13b6813a2b8bcbb5ef6c3042c6211ec55
phiamo <phiamo@googlemail.com>
2012-05-29 13:14:55 +0200

    Added dependend assets check first shot

---
+++ src/Assetic/Filter/DependendFilter.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2012 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+use Assetic\Asset\AssetInterface;
+
+/**
+ * A filter which has can look for dependencies in the assets it manipulates
+ *
+ * @author Philipp A. Mohrenweiser <phiamo@googlemail.com>
+ */
+abstract class DependendFilter implements DependendFilterInterface
+{
+    protected $dependencyFiles = array();
+
+    /**
+     * Check if Content has a dependency
+     * @param AssetInterface $asset
+     */
+    public function hasDependencies(AssetInterface $asset)
+    {
+        $path = $asset->getSourceRoot().DIRECTORY_SEPARATOR.$asset->getSourcePath();
+        $plainAssetContent = file_get_contents($path);
+        return preg_match('/\s*@import.*[\'|\"](.*)[\'|\"].*;\s*/iU', $plainAssetContent);
+    }
+
+    public function getDependencyLastModified(AssetInterface $asset)
+    {
+        //TODO: make this work with remote assets
+
+        $path = $asset->getSourceRoot().DIRECTORY_SEPARATOR.$asset->getSourcePath();
+
+        return $this->getLastModifiedDependencyDate($path);
+    }
+
+    protected function getLastModifiedDependencyDate($path)
+    {
+        $dependendModificationDate = 0;
+        $this->addDependencyFiles($path);
+        foreach ($this->dependencyFiles as $file) {
+            $fileModificationDate = filemtime($file);
+            if ($dependendModificationDate < $fileModificationDate) {
+                $dependendModificationDate = $fileModificationDate;
+            }
+        }
+        return $dependendModificationDate;
+    }
+
+    protected function addDependencyFiles($path)
+    {
+        $files = array();
+        $return = null;
+        //TODO: use asset factory to generate a new asset
+        $plainAssetContent = file_get_contents($path);
+        $baseDir = dirname($path);
+        if ($return = preg_match_all('/\s*@import.*[\'|\"](.*)[\'|\"].*;\s*/iU', $plainAssetContent, $matches)) {
+            foreach ($matches[1] as $file) {
+                $filePath = $baseDir . DIRECTORY_SEPARATOR . $file;
+                if (!in_array($filePath, $this->dependencyFiles)) {
+                    $this->dependencyFiles[] = $filePath;
+                    $this->addDependencyFiles($filePath);
+                }
+            }
+        }
+    }
+}
---
+++ src/Assetic/Filter/DependendFilterInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2012 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+use Assetic\Asset\AssetInterface;
+
+/**
+ * A filter manipulates an asset at load and dump.
+ *
+ * @author Philipp A. Mohrenweiser <phiamo@googlemail.com>
+ */
+interface DependendFilterInterface extends FilterInterface
+{
+    /**
+     * Tells us if the Asset has a dependency this filter wants to know about
+     */
+    function hasDependencies(AssetInterface $asset);
+
+    /**
+     * Get Last Modified Date of Dependencies
+     *
+     * @param AssetInterface $asset An asset
+     */
+    function getDependencyLastModified(AssetInterface $asset);
+}
