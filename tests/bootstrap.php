15ca699154ac8c61178ccc49af51736c5355c1a1
Johannes M. Schmitt <schmittjoh@gmail.com>
2012-07-20 02:40:35 +0200

    fixes tests for latest composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
