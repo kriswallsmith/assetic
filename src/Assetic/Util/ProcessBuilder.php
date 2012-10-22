ecc8a8f791f197dcbdce13e5829e86d9ff7a0289
Stephen Kirkby <stephen.kirkby@orange.com>
2012-10-22 11:02:49 +0100

    Increase timeout to 3 minutes after testing

--- src/Assetic/Util/ProcessBuilder.php
+++ src/Assetic/Util/ProcessBuilder.php
@@ -22,7 +22,7 @@ class ProcessBuilder
     private $cwd;
     private $env;
     private $stdin;
-    private $timeout = 120;
+    private $timeout = 180;
     private $options = array();
     private $inheritEnv = false;
 
