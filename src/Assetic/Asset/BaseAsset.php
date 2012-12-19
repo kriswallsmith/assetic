01eb5ee9723c59ddc232a6bef821cc0ae5f03794
Thomas ROGER <thomaroger@gmail.com>
2012-12-19 15:51:02 +0100

    Update src/Assetic/Asset/BaseAsset.php

--- src/Assetic/Asset/BaseAsset.php
+++ src/Assetic/Asset/BaseAsset.php
@@ -89,6 +89,9 @@ abstract class BaseAsset implements AssetInterface
 
         $filter->filterLoad($asset);
         $this->content = $asset->getContent();
+        if (empty($this->content)) {
+            throw new \RuntimeException('A asset file is empty : '.$_SERVER['REQUEST_URI']);
+        }
 
         $this->loaded = true;
     }
