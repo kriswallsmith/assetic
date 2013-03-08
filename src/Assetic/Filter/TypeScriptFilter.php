9efd8ad4ce49398377416581fb72c3a3d998c246
unknown <CSnodgrass@itlap-christian.internaldrive.local>
2013-03-07 18:24:03 -0800

    Modified TypeScriptFilter to create a temporary file in the temp directory again, but now it will rewrite any references to be relative to the files new location.
    Also added an option which will avoid using the --out flag of the TypeScript compiler if set to false (true is default, which provides original behavior).

--- src/Assetic/Filter/TypeScriptFilter.php
+++ src/Assetic/Filter/TypeScriptFilter.php
@@ -24,11 +24,13 @@ class TypeScriptFilter extends BaseNodeFilter
 {
     private $tscBin;
     private $nodeBin;
+    private $useOut;
 
-    public function __construct($tscBin = '/usr/bin/tsc', $nodeBin = null)
+    public function __construct($tscBin = '/usr/bin/tsc', $nodeBin = null, $useOut = true)
     {
         $this->tscBin = $tscBin;
         $this->nodeBin = $nodeBin;
+        $this->useOut = $useOut;
     }
 
     public function filterLoad(AssetInterface $asset)
@@ -37,26 +39,28 @@ class TypeScriptFilter extends BaseNodeFilter
             ? array($this->nodeBin, $this->tscBin)
             : array($this->tscBin));
 
-        if (null === $asset->getSourcePath()) {
-            $inputDirPath = sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('input_dir');
-            $inputPath = tempnam($inputDirPath, 'ts').'.ts';
+        $inputDirPath = sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('input_dir');
+        mkdir($inputDirPath);
 
-            mkdir($inputDirPath);
-            file_put_contents($inputPath, $asset->getContent());
-        } else {
-            $inputPath = ($asset->getSourceRoot() ? $asset->getSourceRoot() . DIRECTORY_SEPARATOR : '') . $asset->getSourcePath();
+        if (null !== $asset->getSourcePath()) {
+            // Swap out reference path for the appropriate value.
+            $referenceBasePath = '..'.DIRECTORY_SEPARATOR.'..'.dirname(($asset->getSourceRoot() ? $asset->getSourceRoot() . DIRECTORY_SEPARATOR : '') . $asset->getSourcePath());
+            $asset->setContent(preg_replace('/\/\/\/\s*<\s*reference\s*path=[\'"]([^\'^"]*)[\'"]\s*\/>/', "/// <reference path='" . $referenceBasePath.DIRECTORY_SEPARATOR.'$1' . "'/>", $asset->getContent()));
         }
 
-        $outputPath = tempnam(sys_get_temp_dir(), 'output');
-        $pb->add($inputPath)->add('--out')->add($outputPath);
+        $inputPath = $inputDirPath.DIRECTORY_SEPARATOR.uniqid('ts').'.ts';
+        file_put_contents($inputPath, $asset->getContent());
+
+        if ($this->useOut) {
+            $outputPath = tempnam(sys_get_temp_dir(), 'output').'.js';
+            $pb->add($inputPath)->add('--out')->add($outputPath);
+        } else {
+            $outputPath = preg_replace('/\.ts$/', '.js', $inputPath);
+            $pb->add($inputPath);
+        }
 
         $proc = $pb->getProcess();
         $code = $proc->run();
-
-        if (null === $asset->getSourcePath()) {
-            unlink($inputPath);
-            rmdir($inputDirPath);
-        }
 
         if (0 !== $code) {
             if (file_exists($outputPath)) {
@@ -70,7 +74,10 @@ class TypeScriptFilter extends BaseNodeFilter
         }
 
         $compiledJs = file_get_contents($outputPath);
+
         unlink($outputPath);
+        unlink($inputPath);
+        rmdir($inputDirPath);
 
         $asset->setContent($compiledJs);
     }
