ad626d833331aecd06fc297bfe788ae5af2a6387
Matthias Pigulla <mp@webfactory.de>
2013-05-23 15:37:23 +0200

    Add a subclass of CallablesFilter that also implements the DependencyExtractor interface.
    Useful for tests, among other things.

---
+++ src/Assetic/Filter/CallablesDependencyExtractor.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2013 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+use Assetic\Factory\AssetFactory;
+
+/**
+ * Like CallablesFilter, but it is also a DependencyExtractor
+ *
+ * @author Matthias Pigulla <mp@webfactory.de>
+ */
+class CallablesDependencyExtractor extends CallablesFilter implements DependencyExtractorInterface
+{
+
+    protected $extractor;
+
+    public function __construct($loader = null, $dumper = null, $extractor = null)
+    {
+        parent::__construct($loader, $dumper);
+        $this->extractor = $extractor;
+    }
+
+    public function getChildren(AssetFactory $factory, $content, $loadPath = null)
+    {
+        return $this->extractor($factory, $content, $loadPath);
+    }
+
+}
