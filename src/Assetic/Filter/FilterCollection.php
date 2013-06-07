c39c7ee69fc3a27a47cf3aa56a3d32ac92d6cae6
Matthias Pigulla <mp@webfactory.de>
2013-06-07 07:01:29 +0200

    Revert unnecessary change

--- src/Assetic/Filter/FilterCollection.php
+++ src/Assetic/Filter/FilterCollection.php
@@ -18,7 +18,7 @@ use Assetic\Asset\AssetInterface;
  *
  * @author Kris Wallsmith <kris.wallsmith@gmail.com>
  */
-class FilterCollection extends BaseFilter implements \IteratorAggregate, \Countable
+class FilterCollection implements FilterInterface, \IteratorAggregate, \Countable
 {
     private $filters = array();
 
