9b77e13d492015aba23c75e6860e2f16cf318986
Bart van den Burg <bart@samson-it.nl>
2013-07-05 09:58:32 +0200

    renamed $lc (Less compiler) to $sc (Scss compiler)

--- src/Assetic/Filter/ScssphpFilter.php
+++ src/Assetic/Filter/ScssphpFilter.php
@@ -41,18 +41,18 @@ class ScssphpFilter implements DependencyExtractorInterface
 
     public function filterLoad(AssetInterface $asset)
     {
-        $lc = new \scssc();
+        $sc = new \scssc();
         if ($this->compass) {
-            new \scss_compass($lc);
+            new \scss_compass($sc);
         }
         if ($dir = $asset->getSourceDirectory()) {
-            $lc->addImportPath($dir);
+            $sc->addImportPath($dir);
         }
         foreach ($this->importPaths as $path) {
-            $lc->addImportPath($path);
+            $sc->addImportPath($path);
         }
 
-        $asset->setContent($lc->compile($asset->getContent()));
+        $asset->setContent($sc->compile($asset->getContent()));
     }
 
     public function setImportPaths(array $paths)
