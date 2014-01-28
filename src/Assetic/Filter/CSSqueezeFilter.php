6b4208923290391393969ce21856f54b057c1f17
ianbogda <ianbogda@gmail.com>
2014-01-28 01:22:38 +0100

    Add support for CSSqueeze, a CSS minifier that has a compressing ratio comparable to that of csscompressor,YUI Compressor, excssive, cssminifier.
    see http://github.com/ianbogda/CSSqueeze

---
+++ src/Assetic/Filter/CSSqueezeFilter.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2014 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+use Assetic\Asset\AssetInterface;
+
+/**
+ * CSSqueeze filter.
+ *
+ * @link https://github.com/anbogda/CSSqueeze
+ * @author Yann Bogdanovic <ianbogda@gmail.com>
+ */
+class CSSqueezeFilter implements FilterInterface
+{
+    private $singleLine   = true;
+    private $keepHack     = true;
+    private $configuration= array();
+    private $deflatIndent = '';
+
+    public function setSingleLine($bool)
+    {
+        $this->singleLine = (bool) $bool;
+    }
+
+    public function setDeflatIndet($deflatIndent = null)
+    {
+        $this->deflatIndent = (string) $deflatIndent;
+    }
+
+    public function setConfiguration($configuration)
+    {
+        $this->configuration = (array) $configuration;
+    }
+
+    public function keepHack($bool)
+    {
+        $this->keepHack = (bool) $bool;
+    }
+
+    public function filterLoad(AssetInterface $asset)
+    {
+    }
+
+    public function filterDump(AssetInterface $asset)
+    {
+        $parser = new \CSSqueeze(
+			$this->deflatIndent,
+			$this->configuration
+		);
+        $asset->setContent($parser->squeeze(
+            $asset->getContent(),
+            $this->singleLine,
+            $this->keepHack
+        ));
+    }
+}
---
+++ tests/Assetic/Test/Filter/CSSqueezeFilterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2014 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Test\Filter;
+
+use Assetic\Asset\FileAsset;
+use Assetic\Filter\CSSqueezeFilter;
+
+/**
+ * @group integration
+ */
+class CSSqueezeFilterTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        if (!class_exists('CSSqueeze')) {
+            $this->markTestSkipped('CSSqueeze is not installed.');
+        }
+    }
+
+    public function testRelativeSourceUrlImportImports()
+    {
+        $asset = new FileAsset(__DIR__.'/fixtures/cssmin/main.css');
+        $asset->load();
+
+        $filter = new CSSqueezeFilter();
+		$filter->setConfiguration(array('BasePath' => __DIR__.'/fixtures/cssmin/')); 
+        $filter->filterDump($asset);
+
+        $this->assertEquals("body{background:#000;color:#fff}", $asset->getContent());
+    }
+}
