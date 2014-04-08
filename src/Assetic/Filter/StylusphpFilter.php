40f7bc900cc5ce350ce1e335bcb7eddd1fc86f1a
Linus Unnebäck <linus@folkdatorn.se>
2014-04-08 16:56:13 +0200

    filter: new filter Stylusphp

---
+++ src/Assetic/Filter/StylusphpFilter.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2014 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Exception\FilterException;
+use Assetic\Factory\AssetFactory;
+
+use Stylus\Stylus;
+
+/**
+ * Loads STYL files.
+ *
+ * @link https://github.com/AustP/Stylus.php
+ * @author Linus Unnebäck <linus@folkdatorn.se>
+ */
+class StylusphpFilter implements DependencyExtractorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function filterLoad(AssetInterface $asset)
+    {
+        $stylus = new Stylus();
+
+        $stylus->setReadDir($asset->getSourceRoot());
+        $stylus->setImportDir($asset->getSourceDirectory());
+
+        $output = $stylus->parseFileToString($asset->getSourcePath());
+
+        $asset->setContent($output);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterDump(AssetInterface $asset)
+    {
+    }
+
+    public function getChildren(AssetFactory $factory, $content, $loadPath = null)
+    {
+        // todo
+        return array();
+    }
+}
