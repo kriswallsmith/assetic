2fb85f8d99483f3b0f39ab4d17226522beb06c21
Ivan Nikolić <ivan.nikolic@trikoder.net>
2014-10-30 11:23:54 +0100

    Add support for `GEM_GOME` and `GEM_PATH`

---
+++ src/Assetic/Filter/BaseRubyFilter.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Assetic package, an OpenSky project.
+ *
+ * (c) 2010-2014 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Assetic\Filter;
+
+abstract class BaseRubyFilter extends BaseProcessFilter
+{
+    private $gemHome = '';
+    private $gemPaths = array();
+
+    /**
+     * @return string
+     */
+    public function getGemHome()
+    {
+        return $this->gemHome;
+    }
+
+    /**
+     * @param string $gemHome
+     */
+    public function setGemHome($gemHome)
+    {
+        $this->gemHome = $gemHome;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGemPaths()
+    {
+        return $this->gemPaths;
+    }
+
+    /**
+     * @param array $gemPaths
+     */
+    public function setGemPaths(array $gemPaths)
+    {
+        $this->gemPaths = $gemPaths;
+    }
+
+    /**
+     * @param string $gemPath
+     */
+    public function addGemPath($gemPath)
+    {
+        $this->gemPaths[] = $gemPath;
+    }
+
+    protected function createProcessBuilder(array $arguments = array())
+    {
+        $pb = parent::createProcessBuilder($arguments);
+
+        if ($this->gemHome) {
+            $this->mergeEnv($pb);
+            $pb->setEnv('GEM_HOME', $this->gemHome);
+        }
+
+        if ($this->gemPaths) {
+            $this->mergeEnv($pb);
+            $pb->setEnv('GEM_PATH', implode(':', $this->gemPaths));
+        }
+
+        return $pb;
+    }
+}
