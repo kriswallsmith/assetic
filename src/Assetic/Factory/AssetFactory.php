3be67aadf34184a3c7ae2db33cb6abf2e18c8fba
Issei.M <issei.m7@gmail.com>
2015-07-02 22:54:55 +0900

    AssetFactory::createAsset() now generates before initialize "root" option by default

--- src/Assetic/Factory/AssetFactory.php
+++ src/Assetic/Factory/AssetFactory.php
@@ -172,6 +172,10 @@ class AssetFactory
             $options['debug'] = $this->debug;
         }
 
+        if (!isset($options['name'])) {
+            $options['name'] = $this->generateAssetName($inputs, $filters, $options);
+        }
+
         if (!isset($options['root'])) {
             $options['root'] = array($this->root);
         } else {
@@ -180,10 +184,6 @@ class AssetFactory
             }
 
             $options['root'][] = $this->root;
-        }
-
-        if (!isset($options['name'])) {
-            $options['name'] = $this->generateAssetName($inputs, $filters, $options);
         }
 
         $asset = $this->createAssetCollection(array(), $options);
