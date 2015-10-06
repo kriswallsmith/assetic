78a871bbe7262bdcd32a022ee6b65c0e3b9fe47b
Alex Ash <streamprop@gmail.com>
2015-10-06 19:07:28 +0300

    Implement correct comment filtering for LESS refs #758

--- src/Assetic/Util/LessUtils.php
+++ src/Assetic/Util/LessUtils.php
@@ -21,4 +21,6 @@ abstract class LessUtils extends CssUtils
     const REGEX_IMPORTS         = '/@import(?:-once)? (?:\([a-z]*\) )?(?:url\()?(\'|"|)(?P<url>[^\'"\)\n\r]*)\1\)?;?/';
     const REGEX_IMPORTS_NO_URLS = '/@import(?:-once)? (?:\([a-z]*\) )?(?!url\()(\'|"|)(?P<url>[^\'"\)\n\r]*)\1;?/';
     const REGEX_COMMENTS        = '/((?:\/\*[^*]*\*+(?:[^\/][^*]*\*+)*\/)|\/\/[^\n]+)/';
+
+    const ANALYZER = '\Assetic\Util\Analyzer\LessAnalyzer';
 }
