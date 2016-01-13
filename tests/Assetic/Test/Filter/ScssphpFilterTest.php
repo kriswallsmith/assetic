b8168a0c4375c1b5919c072eac32d4a355bbcabe
Andrew Tch <andrew.tchircoff@gmail.com>
2016-01-13 10:12:49 +0200

    initial push for testing compatibility

--- tests/Assetic/Test/Filter/ScssphpFilterTest.php
+++ tests/Assetic/Test/Filter/ScssphpFilterTest.php
@@ -90,9 +90,9 @@ EOF;
 
     public function testCompassExtensionCanBeDisabled()
     {
-        $this->setExpectedExceptionRegExp(
+        $this->setExpectedException(
             'Exception',
-            '/Undefined mixin box\-shadow\: failed at `@include box\-shadow\(10px 10px 8px red\);`.*? line:? 4/'
+            'Undefined mixin box-shadow: line: 4'
         );
 
         $asset = new FileAsset(__DIR__.'/fixtures/sass/main_compass.scss');
