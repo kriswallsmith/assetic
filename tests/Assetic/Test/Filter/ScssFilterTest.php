f32ebe1fdeec2eeb77b2a45fddd28e6c32373f06
Jack Wilkinson <me@jackwilky.com>
2019-08-09 23:38:16 +0100

    Updated tests for ScssFilter for package update

--- tests/Assetic/Test/Filter/ScssFilterTest.php
+++ tests/Assetic/Test/Filter/ScssFilterTest.php
@@ -23,8 +23,8 @@ class ScssFilterTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
-        if (!class_exists('Leafo\ScssPhp\Compiler')) {
-            $this->markTestSkipped('leafo/scssphp is not installed');
+        if (!class_exists('ScssPhp\ScssPhp\Compiler')) {
+            $this->markTestSkipped('scssphp/scssphp is not installed');
         }
     }
 
@@ -93,7 +93,7 @@ EOF;
         $actual->load();
 
         $filter = $this->getFilter();
-        $filter->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
+        $filter->setFormatter('ScssPhp\ScssPhp\Formatter\Compressed');
         $filter->filterLoad($actual);
 
         $this->assertRegExp(
