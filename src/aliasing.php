b91cda20ae026d0b5b321ceb4b9adbe258863596
Jack Wilkinson <me@jackwilky.com>
2020-06-09 14:30:02 +0100

    Added class aliasing for migrated intefaces for v2

---
+++ src/aliasing.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Class aliasing allowing for easier migration from v1 to v2.
+ *
+ * @author Jack Wilkinson <me@jackwilky.com>
+ */
+
+class_alias(
+    Assetic\Contracts\Asset\AssetInterface::class,
+    Assetic\Asset\AssetInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Asset\AssetCollectionInterface::class,
+    Assetic\Asset\AssetCollectionInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Cache\CacheInterface::class,
+    Assetic\Cache\CacheInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Exception\Exception::class,
+    Assetic\Exception\Exception::class
+);
+
+class_alias(
+    Assetic\Contracts\Factory\Loader\FormulaLoaderInterface::class,
+    Assetic\Factory\Loader\FormulaLoaderInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Factory\Resource\IteratorResourceInterface::class,
+    Assetic\Resource\IteratorResourceInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Factory\Resource\ResourceInterface::class,
+    Assetic\Resource\ResourceInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Factory\Worker\WorkerInterface::class,
+    Assetic\Worker\WorkerInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Filter\DependencyExtractorInterface::class,
+    Assetic\Filter\DependencyExtractorInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Filter\FilterInterface::class,
+    Assetic\Filter\FilterInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\Filter\HashableInterface::class,
+    Assetic\Filter\HashableInterface::class
+);
+
+class_alias(
+    Assetic\Contracts\ValueSupplierInterface::class,
+    Assetic\ValueSupplierInterface::class
+);
